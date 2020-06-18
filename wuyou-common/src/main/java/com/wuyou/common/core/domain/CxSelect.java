52f8073829e78cc75cd96d90024ee03f2824541f
duanluan <duanluan@outlook.com>
2020-06-18 19:53:22 +0800

    fork: 2020-06-02 004e5b8 新增多级联动下拉示例

---
+++ wuyou-common/src/main/java/com/wuyou/common/core/domain/CxSelect.java
@@ -0,0 +1,26 @@
+package com.wuyou.common.core.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CxSelect {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 数据值字段名称
+   */
+  private String v;
+
+  /**
+   * 数据标题字段名称
+   */
+  private String n;
+
+  /**
+   * 子集数据字段名称
+   */
+  private List<CxSelect> s;
+}
