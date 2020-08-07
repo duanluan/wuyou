4347a59b39969bb81a3edfbd00c2897b85072ffa
duanluan <duanluan@outlook.com>
2020-08-07 16:31:22 +0800

    fork: 2020-07-04 7728ad9 检查字符支持小数点 & 降级改成异常提醒

--- wuyou-common/src/main/java/com/wuyou/common/utils/sql/SqlUtil.java
+++ wuyou-common/src/main/java/com/wuyou/common/utils/sql/SqlUtil.java
@@ -1,25 +1,26 @@
 package com.wuyou.common.utils.sql;
 
 
+import com.wuyou.common.exception.base.BaseException;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- * sql操作工具类
+ * sql 操作工具类
  *
  * @author wuyou
  */
 public class SqlUtil {
   /**
-   * 仅支持字母、数字、下划线、空格、逗号（支持多个字段排序）
+   * 仅支持字母、数字、下划线、空格、逗号、小数点（支持多个字段排序）
    */
-  public static String SQL_PATTERN = "[a-zA-Z0-9_\\ \\,]+";
+  public static String SQL_PATTERN = "[a-zA-Z0-9_\\ \\,\\.]+";
 
   /**
    * 检查字符，防止注入绕过
    */
   public static String escapeOrderBySql(String value) {
     if (StringUtils.isNotEmpty(value) && !isValidOrderBySql(value)) {
-      return StringUtils.EMPTY;
+      throw new BaseException("参数不符合规范，不能进行查询");
     }
     return value;
   }
