f4ccedb3eabf32855aa6ef92d9a5fd85b6b6f8d3
duanluan <duanluan@outlook.com>
2020-08-08 16:00:27 +0800

    fork: 2020-08-03 8bf5140 用户密码支持自定义配置规则

--- wuyou-admin/src/main/resources/static/js/common.js
+++ wuyou-admin/src/main/resources/static/js/common.js
@@ -350,6 +350,38 @@ function calSumWidth(elements) {
   return width;
 }
 
+/**
+ * 密码规则范围验证
+ *
+ * @param chrtype 密码规则
+ * @param password 密码
+ * @returns {boolean}
+ */
+function checkpwd(chrtype, password) {
+  if (chrtype == 1) {
+    if (!$.common.numValid(password)) {
+      $.modal.alertWarning("密码只能为 0-9 数字");
+      return false;
+    }
+  } else if (chrtype == 2) {
+    if (!$.common.enValid(password)) {
+      $.modal.alertWarning("密码只能为 a-z 和 A-Z 字母");
+      return false;
+    }
+  } else if (chrtype == 3) {
+    if (!$.common.enNumValid(password)) {
+      $.modal.alertWarning("密码必须包含字母以及数字");
+      return false;
+    }
+  } else if (chrtype == 4) {
+    if (!$.common.charValid(password)) {
+      $.modal.alertWarning("密码必须包含字母、数字、以及特殊符号-、_");
+      return false;
+    }
+  }
+  return true;
+}
+
 // 日志打印封装处理
 var log = {
   log: function (msg) {
