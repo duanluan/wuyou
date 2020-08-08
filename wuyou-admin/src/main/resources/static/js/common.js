32903f37176ca7f978d11bb76eb802449c38d125
duanluan <duanluan@outlook.com>
2020-08-08 14:12:09 +0800

    fork: 2020-07-26 d7f7da5 openTab 打开时滚动到当前页签

--- wuyou-admin/src/main/resources/static/js/common.js
+++ wuyou-admin/src/main/resources/static/js/common.js
@@ -278,6 +278,7 @@ function createMenuItem(dataUrl, menuName) {
     if ($(this).data('id') == dataUrl) {
       if (!$(this).hasClass('active')) {
         $(this).addClass('active').siblings('.menuTab').removeClass('active');
+        scrollToTab(this);
         $('.page-tabs-content').animate({ marginLeft: "" }, "fast");
         // 显示tab对应的内容区
         $('.mainContent .wuyou_iframe', topWindow).each(function () {
@@ -296,7 +297,7 @@ function createMenuItem(dataUrl, menuName) {
     var str = '<a href="javascript:;" class="active menuTab" data-id="' + dataUrl + '" data-panel="' + panelUrl + '">' + menuName + ' <i class="fa fa-times-circle"></i></a>';
     $('.menuTab', topWindow).removeClass('active');
 
-    // 添加选项卡对应的iframe
+    // 添加选项卡对应的 iframe
     var str1 = '<iframe class="wuyou_iframe" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" data-panel="' + panelUrl + '" seamless></iframe>';
     $('.mainContent', topWindow).find('iframe.wuyou_iframe').hide().parents('.mainContent').append(str1);
 
@@ -307,8 +308,46 @@ function createMenuItem(dataUrl, menuName) {
 
     // 添加选项卡
     $('.menuTabs .page-tabs-content', topWindow).append(str);
+    scrollToTab($('.menuTab.active', topWindow));
   }
   return false;
+}
+
+// 滚动到指定选项卡
+function scrollToTab(element) {
+  var topWindow = $(window.parent.document);
+  var marginLeftVal = calSumWidth($(element).prevAll()),
+    marginRightVal = calSumWidth($(element).nextAll());
+  // 可视区域非 tab 宽度
+  var tabOuterWidth = calSumWidth($(".content-tabs", topWindow).children().not(".menuTabs"));
+  // 可视区域 tab 宽度
+  var visibleWidth = $(".content-tabs", topWindow).outerWidth(true) - tabOuterWidth;
+  // 实际滚动宽度
+  var scrollVal = 0;
+  if ($(".page-tabs-content", topWindow).outerWidth() < visibleWidth) {
+    scrollVal = 0;
+  } else if (marginRightVal <= (visibleWidth - $(element).outerWidth(true) - $(element).next().outerWidth(true))) {
+    if ((visibleWidth - $(element).next().outerWidth(true)) > marginRightVal) {
+      scrollVal = marginLeftVal;
+      var tabElement = element;
+      while ((scrollVal - $(tabElement).outerWidth()) > ($(".page-tabs-content", topWindow).outerWidth() - visibleWidth)) {
+        scrollVal -= $(tabElement).prev().outerWidth();
+        tabElement = $(tabElement).prev();
+      }
+    }
+  } else if (marginLeftVal > (visibleWidth - $(element).outerWidth(true) - $(element).prev().outerWidth(true))) {
+    scrollVal = marginLeftVal - $(element).prev().outerWidth(true);
+  }
+  $('.page-tabs-content', topWindow).animate({ marginLeft: 0 - scrollVal + 'px' }, "fast");
+}
+
+// 计算元素集合的总宽度
+function calSumWidth(elements) {
+  var width = 0;
+  $(elements).each(function () {
+    width += $(this).outerWidth(true);
+  });
+  return width;
 }
 
 // 日志打印封装处理
