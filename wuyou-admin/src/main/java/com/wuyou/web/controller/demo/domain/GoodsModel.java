0489a6a3a993d833aae24266c838df1e68ddab76
duanluan <duanluan@outlook.com>
2020-08-12 13:25:20 +0800

    fork: 2020-08-11 a0ada99 主子表示例添加日期格式案例

--- wuyou-admin/src/main/java/com/wuyou/web/controller/demo/domain/GoodsModel.java
+++ wuyou-admin/src/main/java/com/wuyou/web/controller/demo/domain/GoodsModel.java
@@ -2,6 +2,8 @@ package com.wuyou.web.controller.demo.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 商品测试信息
  *
@@ -13,17 +15,18 @@ public class GoodsModel {
    * 商品名称
    */
   private String name;
-
+  /**
+   * 商品日期
+   */
+  private Date date;
   /**
    * 商品重量
    */
   private Integer weight;
-
   /**
    * 商品价格
    */
   private Double price;
-
   /**
    * 商品种类
    */
