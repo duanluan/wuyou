9388131c5a7299963bfd6ef675216329919f0b72
duanluan <duanluan@outlook.com>
2021-04-10 20:02:20 +0800

    fork: 2020-08-26 cc35627 添加 Ajax 局部刷新 demo

--- wuyou-admin/src/main/java/com/wuyou/web/controller/demo/controller/DemoFormController.java
+++ wuyou-admin/src/main/java/com/wuyou/web/controller/demo/controller/DemoFormController.java
@@ -1,14 +1,18 @@
 package com.wuyou.web.controller.demo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.wuyou.common.core.domain.CxSelect;
 import com.wuyou.common.core.domain.Result;
+import com.wuyou.common.utils.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -203,6 +207,46 @@ public class DemoFormController {
 
     mmap.put("data", JSON.toJSON(cxList));
     return PREFIX + "/cxselect";
+  }
+
+  /**
+   * 局部刷新
+   */
+  @GetMapping("/localrefresh")
+  public String localRefresh(ModelMap mmap) {
+    JSONArray list = new JSONArray();
+    JSONObject item = new JSONObject();
+    item.put("name", "这条任务数据是由ModelMap传递到页面的，点击添加按钮后会将这条数据替换为新数据");
+    item.put("type", "默认");
+    item.put("date", "2020.06.10");
+    list.add(item);
+    mmap.put("tasks", list);
+    mmap.put("min", 2);
+    mmap.put("max", 10);
+    return PREFIX + "/localrefresh";
+  }
+
+  /**
+   * 局部刷新-添加任务
+   *
+   * @param  fragment  页面中的模板名称
+   * @param  taskName  任务名称
+   */
+  @PostMapping("/localrefresh/task")
+  public String localRefreshTask(String fragment, String taskName, ModelMap mmap) {
+    JSONArray list = new JSONArray();
+    JSONObject item = new JSONObject();
+    item.put("name", StringUtils.defaultIfBlank(taskName, "通过电话销售过程中了解各盛市的设备仪器使用、采购情况及相关重要追踪人"));
+    item.put("type", "新增");
+    item.put("date", "2018.06.10");
+    list.add(item);
+    item = new JSONObject();
+    item.put("name", "提高自己电话营销技巧，灵活专业地与客户进行电话交流");
+    item.put("type", "新增");
+    item.put("date", "2018.06.12");
+    list.add(item);
+    mmap.put("tasks", list);
+    return PREFIX + "/localrefresh::" + fragment;
   }
 
   /**
