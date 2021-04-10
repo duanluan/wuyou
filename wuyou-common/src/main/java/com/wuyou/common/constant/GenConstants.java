71bcdc3b52e308309e6249b0cc0e7c6796e82f0a
duanluan <duanluan@outlook.com>
2021-04-10 20:15:23 +0800

    fork: 2020-08-28 118cd15 修正数据库字符串类型 nvarchar

--- wuyou-common/src/main/java/com/wuyou/common/constant/GenConstants.java
+++ wuyou-common/src/main/java/com/wuyou/common/constant/GenConstants.java
@@ -41,7 +41,7 @@ public class GenConstants {
   /**
    * 数据库字符串类型
    */
-  public static final String[] COLUMNTYPE_STR = {"char", "varchar", "narchar", "varchar2", "tinytext", "text", "mediumtext", "longtext"};
+  public static final String[] COLUMNTYPE_STR = {"char", "varchar", "nvarchar", "varchar2", "tinytext", "text", "mediumtext", "longtext"};
   /**
    * 数据库时间类型
    */
